c44363bd3ac45a1dc3fd520ab3e70dce6b22368d
David Cappel <dcappel15@gmail.com>
2025-04-01 10:05:48 -0400

    style changes to firstmon

--- apps/firstmon/src/app/page.tsx
+++ apps/firstmon/src/app/page.tsx
@@ -8,20 +8,20 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-    <div className=" items-center justify-items-center min-h-screen p-8 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-amber-500">
+    <div className=" items-center justify-items-center min-h-screen p-8 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-gradient-to-br from-amber-500 to-slate-500 via-white">
       <div className="bg-white p-2 px-5 m-2 ring-1 ring-red-400 shadow-2xl shadow-gray-400">
         <div className="bg-gradient-to-r from-slate-700 to-amber-800 via-amber-500 text-transparent bg-clip-text font-bold text-5xl">
           My first Monorepo
         </div>
 
       </div>
-      <div className=" mt-11">
-        <div>
-          <Button asChild className=" hover:scale-105">
-          <Link href ="https://idavid-ten.vercel.app/aboutMe" target="_blank" rel="noopener noreferrer"> My Journery</Link>
-        </Button>
+      <div className="">
+        <div className="mt-11 flex justify-center">
+          <Button asChild className=" hover:scale-105 shadow-xl ring-1 ring-white">
+            <Link href ="https://idavid-ten.vercel.app/aboutMe" target="_blank" rel="noopener noreferrer"> My Journery</Link>
+          </Button>
         </div>
-        <div className="mt-10">
+        <div className="mt-10 bg-white p-6 m-4 rounded-sm ring-1 shadow-xl flex justify-center">
           <ProfileForm></ProfileForm>
           
         </div>       
